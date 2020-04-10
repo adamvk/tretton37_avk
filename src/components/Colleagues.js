62a7a5e0dd0c52a112f65d0d51ee05b3ef41f617
Adam von Kraemer <adam.vk@live.se>
2020-04-10 11:27:17 +0200

    added social media links

--- src/components/Colleagues.js
+++ src/components/Colleagues.js
@@ -3,10 +3,16 @@ import "./styles.css";
 import { Input, Card } from "semantic-ui-react";
 import axios from "axios";
 
+import gitHub from "../images/github.png";
+import linkedIn from "../images/linkedin.png";
+import stackOverflow from "../images/stackoverflow.png";
+import twitter from "../images/twitter.png";
+
 const Colleagues = () => {
     const [colleagues, setColleagues] = useState([]);
 
     useEffect( () => {
+        // fetch all colleagues by making API call
         axios.get('https://api.tretton37.com/ninjas')
             .then( res => {
                 console.log(res.data)
@@ -15,6 +21,7 @@ const Colleagues = () => {
     }, []);
 
     const renderColleagues = () => (
+        // render all colleagues as cards
         <Card.Group style={{marginTop: "2%"}}>
             { colleagues.map(colleague => (
                 <Card className="card">
@@ -29,6 +36,28 @@ const Colleagues = () => {
                             <div>
                                 Office: { colleague.office }
                             </div>
+                        </div>
+                        <div className="card-symbols">
+                            { colleague.linkedIn &&
+                                <a href={`https://linkedin.com${colleague.linkedIn}`} target="_blank" rel="noopener noreferrer">
+                                    <img src={linkedIn} className="card-symbol" alt="LinkedIn" />
+                                </a>
+                            }
+                            { colleague.gitHub &&
+                                <a href={`https://github.com/${colleague.gitHub}`} target="_blank" rel="noopener noreferrer">
+                                    <img src={gitHub} className="card-symbol" alt="GitHub" />
+                                </a>
+                            }
+                            { colleague.twitter &&
+                                <a href={`https://twitter.com/${colleague.twitter}`} target="_blank" rel="noopener noreferrer">
+                                    <img src={twitter} className="card-symbol" alt="Twitter" />
+                                </a>
+                            }
+                            { colleague.stackOverflow &&
+                                <a href={`https://stackoverflow.com/users/${colleague.stackOverflow}`} target="_blank" rel="noopener noreferrer">
+                                    <img src={stackOverflow} className="card-symbol" alt="StackOverflow" />
+                                </a>
+                            }
                         </div>
                     </Card.Content>
                 </Card>
